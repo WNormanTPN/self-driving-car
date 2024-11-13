ea41dcb3c6409d9ce003c858a3aac9b389f78b05
Norman <vinhstarmcpc@gmail.com>
2024-11-13 07:49:55 +0700

    Add: main.py to run model

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import base64
 from datetime import datetime
 import os
 import shutil
+from src import *
 
+import json
+import asyncio
 import numpy as np
-import socketio
-import eventlet
-import eventlet.wsgi
+import websockets
 from PIL import Image
 from flask import Flask
 from io import BytesIO
@@ -16,8 +17,10 @@ from tensorflow.keras.models import load_model
 from keras.config import enable_unsafe_deserialization
 from keras import __version__ as keras_version
 
-sio = socketio.Server()
-app = Flask(__name__)
+ENGINE_PACKET_TYPE_MESSAGE = 4
+SOCKET_PACKET_TYPE_EVENT = 2
+
+target_size = (66, 200)
 model = None
 prev_image_array = None
 
@@ -48,54 +51,91 @@ set_speed = 9
 controller.set_desired(set_speed)
 
 
-@sio.on('telemetry')
-def telemetry(sid, data):
-    print("telemetry")
+# Hàm xử lý khi nhận kết nối từ client
+async def handle_connection(websocket):
+    try:
+        # Listen for data from the client
+        async for message in websocket:
+            try:
+                data = json.loads(message[2:])  # Parse JSON
+                event_name = data[0]
+                data =data[1]
+                if event_name == "telemetry":
+                    # Parse received data
+                    steering_angle = data["steering_angle"]
+                    throttle = data["throttle"]
+                    speed = data["speed"]
+                    imgString = data["image"]
+
+                    # Decode image from base64
+                    image = Image.open(BytesIO(base64.b64decode(imgString)))
+                    image_array = np.asarray(image)
+
+                    # Preprocess image
+                    preprocessed_image = preprocess_image(image_array)
+
+                    # Reshape for prediction (model expects batch size of 1)
+                    preprocessed_image = np.expand_dims(preprocessed_image, axis=0)
+
+                    # Predict steering angle
+                    steering_angle = float(model.predict(preprocessed_image, batch_size=1))
+
+                    # Update throttle using controller
+                    throttle = 10
+
+                    # Print and send response back to the client
+                    print(steering_angle, throttle)
+                    await send_control(websocket, steering_angle, throttle)
+
+                    # Save frame if image_folder is specified
+                    if args.image_folder != '':
+                        timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+                        image_filename = os.path.join(args.image_folder, timestamp)
+                        image.save('{}.jpg'.format(image_filename))
+            except json.JSONDecodeError:
+                continue
+    except websockets.exceptions.ConnectionClosed as e:
+        return
+
+
+async def emit(websocket, event, data=None, packet_id=0, namespace="/"):
+    message_type = f"{ENGINE_PACKET_TYPE_MESSAGE}{SOCKET_PACKET_TYPE_EVENT}"
+    payload = [event]
+    
     if data:
-        print("telemetry")
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = data["speed"]
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        payload.append(data)
 
-        throttle = controller.update(float(10))
+    # Thêm packet_id vào gói tin nếu có
+    if packet_id >= 0:
+        message_type += str(packet_id)
 
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
+    # Xây dựng namespace nếu có
+    if namespace and namespace != "/":
+        message_type += namespace + ","
+    
+    # Chuyển payload thành JSON string
+    message = message_type + json.dumps(payload)
 
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
-    else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+    # Gửi message qua WebSocket
+    await websocket.send(message)
 
+async def send_control(websocket, steering_angle, throttle):
+    # Định nghĩa dữ liệu điều khiển
+    control_data = {
+        "steering_angle": str(steering_angle),
+        "throttle": str(throttle)
+    }
+    
+    await emit(websocket, "steer", data=control_data)
+    print("Control data sent")
+    
 
-@sio.on('connect')
-def connect(sid, environ):
-    print("connect ", sid)
-    send_control(0, 0)
-
-
-def send_control(steering_angle, throttle):
-    print("send_control")
-    sio.emit(
-        "steer",
-        data={
-            'steering_angle': steering_angle.__str__(),
-            'throttle': throttle.__str__()
-        },
-        skip_sid=True)
-
+# Hàm main để chạy WebSocket server
+async def main():
+    # Chạy server WebSocket tại địa chỉ ws://localhost:4567
+    server = await websockets.serve(handle_connection, "localhost", 4567)
+    print("WebSocket server started on ws://localhost:4567")
+    await server.wait_closed()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
@@ -112,15 +152,6 @@ if __name__ == '__main__':
         help='Path to image folder. This is where the images from the run will be saved.'
     )
     args = parser.parse_args()
-
-    # check that model Keras version is same as local Keras version
-    # f = h5py.File(args.model, mode='r')
-    # model_version = f.attrs.get('keras_version')
-    # keras_version = str(keras_version).encode('utf8')
-
-    # if model_version != keras_version:
-    #     print('You are using Keras version ', keras_version,
-    #           ', but the model was built using ', model_version)
     
     enable_unsafe_deserialization()
     model = load_model(args.model)
@@ -135,9 +166,6 @@ if __name__ == '__main__':
         print("RECORDING THIS RUN ...")
     else:
         print("NOT RECORDING THIS RUN ...")
-
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    
+    # Chạy server
+    asyncio.run(main())
